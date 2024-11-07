121791040d4b36d3b13108377f4a0b0088f229fe
NicowaxDelux <158650302+NicowaxDelux@users.noreply.github.com>
2024-11-07 00:31:13 -0500

    Feature/cpg 24/create order details crud and service (#17)
    * create Order Detail service
    
    * create validations for order detail
    
    * create order deatil controller

---
+++ La-Mula-App-main/src/main/kotlin/com/lamulaapp/controller/OrderDetailController.kt
@@ -0,0 +1,56 @@
+package com.lamulaapp.controller
+
+import com.lamulaapp.controller.dto.OrderDetailDto
+import com.lamulaapp.service.OrderDetailService
+import com.lamulaapp.controller.utils.validateCreateOrderDetail
+import com.lamulaapp.controller.utils.validateUpdateOrderDetail
+import com.lamulaapp.exception.ValidationErrorsException
+import org.flywaydb.core.internal.util.IOUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+class OrderDetailController(
+    private val orderDetailService: OrderDetailService
+) {
+
+    @PostMapping("/ordersDetails")
+    fun createOrderDetail(@RequestBody orderDetailDto: OrderDetailDto): ResponseEntity<OrderDetailDto> {
+        val validation = validateCreateOrderDetail(orderDetailDto)
+
+        if (!validation.isValid) {
+            throw ValidationErrorsException(validation.errors)
+        }
+
+        return ResponseEntity(orderDetailService.createOrderDetail(orderDetailDto), HttpStatus.CREATED)
+    }
+
+    @GetMapping("/ordersDetails")
+    fun getAllOrderDetail():ResponseEntity<List<OrderDetailDto>> {
+        return ResponseEntity(orderDetailService.getOrderDetails(), HttpStatus.OK)
+    }
+
+    @GetMapping("/ordersDetails/{id}")
+    fun getOrderDetailById(@PathVariable("id") id: UUID): ResponseEntity<OrderDetailDto> {
+        return ResponseEntity(orderDetailService.getOrderDetailById(id), HttpStatus.OK)
+    }
+
+    @PutMapping("/ordersDetails/{id}")
+    fun updateOrderDetail(@PathVariable("id") id: UUID, @RequestBody orderDetailDto: OrderDetailDto): ResponseEntity<OrderDetailDto> {
+        val validate = validateUpdateOrderDetail(orderDetailDto)
+
+        if(!validate.isValid) {
+            throw ValidationErrorsException(validate.errors)
+        }
+
+        return ResponseEntity(orderDetailService.updateOrderDetail(id, orderDetailDto), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/ordersDetails/{id}")
+    fun deleteOrderDetail(@PathVariable("id") id: UUID): ResponseEntity<Unit> {
+        return ResponseEntity(orderDetailService.deleteOrderDetail(id), HttpStatus.NO_CONTENT)
+    }
+
+}
--- La-Mula-App-main/src/main/kotlin/com/lamulaapp/service/OrderDetailService.kt
+++ La-Mula-App-main/src/main/kotlin/com/lamulaapp/service/OrderDetailService.kt
@@ -1,10 +1,14 @@
 package com.lamulaapp.service
 
 import com.lamulaapp.controller.dto.OrderDetailDto
+import com.lamulaapp.repository.OrderDetailRepository
 import com.lamulaapp.controller.mapper.toDto
 import com.lamulaapp.controller.mapper.toEntity
-import com.lamulaapp.repository.OrderDetailRepository
+import com.lamulaapp.exception.DuplicateKeyException
+import com.lamulaapp.exception.KeysAreDifferentException
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.*
 
 @Service
@@ -13,6 +17,12 @@ class OrderDetailService(
 ) {
 
     fun createOrderDetail(orderDetailDto: OrderDetailDto): OrderDetailDto {
+        val findById = orderDetailDto.idOrderDetail?.let { orderDetailRepository.findById(it)}
+
+        if (findById != null && findById.isPresent) {
+            throw DuplicateKeyException("This ID already exist.")
+        }
+
         val response = orderDetailRepository.save(orderDetailDto.toEntity())
         return response.toDto()
     }
@@ -23,27 +33,43 @@ class OrderDetailService(
 
     fun getOrderDetailById(id: UUID): OrderDetailDto? {
         val response = orderDetailRepository.findById(id)
-        return if (response.isPresent) {
-            response.get().toDto()
-        } else {
-            null
+
+        if (!response.isPresent) {
+            throw EntityNotFoundException("Sorry, this ID doesn't exist.")
         }
+
+        return response.get().toDto()
     }
 
     fun updateOrderDetail(id : UUID, orderDetailDto: OrderDetailDto): OrderDetailDto? {
         val response = orderDetailRepository.findById(id)
 
         if (!response.isPresent) {
-            return null
+            throw EntityNotFoundException("This ID doesn't exist, please try again.")
         }
 
         if (id != orderDetailDto.idOrderDetail) {
-            return null
+            throw KeysAreDifferentException("This ID doesn't exist, please try again.")
         }
-        return orderDetailRepository.save(orderDetailDto.toEntity()).toDto()
+
+        if (orderDetailDto.updatedBy == null) {
+            throw IllegalArgumentException("The field 'updatedBy' is mandatory!.")
+        }
+
+        val updateDate = orderDetailDto
+            .toEntity()
+            .copy(updatedAt = LocalDateTime.now(), updatedBy = orderDetailDto.updatedBy)
+
+        return orderDetailRepository.save(updateDate).toDto()
     }
 
     fun deleteOrderDetail(id: UUID) {
+        val findById = orderDetailRepository.findById(id)
+
+        if (!findById.isPresent) {
+            throw EntityNotFoundException("This ID doesn't exist, please try again.")
+        }
+
         orderDetailRepository.deleteById(id)
     }
 }
