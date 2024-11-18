3eb325bf187fd6bd1ad259868d169c0ae1d9405e
Vanessa Requejo Porras <vnporras1028@gmail.com>
2024-11-18 10:48:17 -0500

    It implemented the config cross-origin.

---
+++ La-Mula-App-main/src/main/kotlin/com/lamulaapp/config/CrossOrigin.kt
@@ -0,0 +1,30 @@
+package com.lamulaapp.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CrossOriginConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+            }
+        }
+    }
+}
+
+//@Configuration
+//class CrossOriginConfig : WebMvcConfigurer {
+//    override fun addCorsMappings(registry: CorsRegistry) {
+//        registry.addMapping("/**")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE")
+//    }
+//}
